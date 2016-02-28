dc20621220fb4c4eca4ad4a2967f788887b88135
Sosloow <sosloow@gmail.com>
2016-02-28 20:58:43 +0500

    docs

--- api_project.js
+++ api_project.js
@@ -1,12 +1,14 @@
 define({
-  "name": "kik-server",
-  "version": "0.0.1",
-  "description": "API for KiK accounting app",
+  "name": "Сервер КиК",
+  "version": "0.1.0",
+  "description": "Документация API учетной системы \"Кофе и Книги\"",
+  "title": "API КиК",
+  "url": "http://kofe-i-knigi.github.io/kik-server/",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-02-28T15:51:23.184Z",
+    "time": "2016-02-28T15:57:34.001Z",
     "url": "http://apidocjs.com",
     "version": "0.15.1"
   }
