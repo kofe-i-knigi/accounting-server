e9499602c97ff3fcfb24d161fa8ce703f659d8fe
Sosloow <sosloow@gmail.com>
2016-03-07 02:17:23 +0500

    add env vars for production

--- config/index.js
+++ config/index.js
@@ -6,9 +6,9 @@ module.exports = {
   },
 
   production: {
-    dbUri: 'postgres://postgres:password@localhost:5432/kik',
-    jwtSecret: 'sdfsd0fj3mvqvmq29835yq3znrupa93rspv95sesp45naa3n4',
-    registrationToken: 'asd0345jmsdfmp03j45fp3qufnsdparmap3854adsp89'
+    dbUri: process.env.DATABASE_URL,
+    jwtSecret: process.env.JWT,
+    registrationToken: process.env.REG_TOKEN
   },
   admin: {
       password: 'lady8ug'
